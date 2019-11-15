b692e83ecae87ae7de8459be381fe5a1e5240155
Matt Johnson <majikjohnson@hotmail.com>
2019-11-15 15:50:42 +0000

    Created basic framework

---
+++ src/main/java/webdriver/DriverType.java
@@ -0,0 +1,4 @@
+package webdriver;
+
+public enum DriverType {
+}
---
+++ src/main/java/webdriver/FirefoxDriverManager.java
@@ -0,0 +1,11 @@
+package webdriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeDriverManager extends DriverManager {
+    @Override
+    protected void createWebDriver() {
+        driver = new ChromeDriver();
+        setTimeout();
+    }
+}
